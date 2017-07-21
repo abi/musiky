36a2a1dbeb906fcb14e91ede6428280d17ee7e0f
Abi Raja <abimanyuraja@gmail.com>
2017-07-21 09:33:47 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+var colors = [
+  '#FF482F',
+  '#F8CE64',
+  '#74F8B7',
+  '#ADC96C',
+  '#FF9B31',
+  '#FF5677',
+  '#BB60BF',
+  '#54F393'
+];
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flexWrap: 'wrap'}}>
+        {
+          colors.map((color, i) =>
+            <View key={i} style={{width: Dimensions.get('window').width / 2, height: Dimensions.get('window').height / 4, backgroundColor: color}}></View>
+          )
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
