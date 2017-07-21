bee14c457db05f0b559cf7cc8fd9671e777b7d31
Abi Raja <abimanyuraja@gmail.com>
2017-07-21 10:17:29 -0700

    Convert to Tile component

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ var colors = [
   '#54F393',
 ];
 
-export default class App extends React.Component {
+class Tile extends React.Component {
   state = { transformed: false };
 
   _onPressIn() {
@@ -32,33 +32,29 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={{ flexWrap: 'wrap' }}>
-        <StatusBar hidden={true} />
-        {colors.map((color, i) =>
-          <TouchableWithoutFeedback
-            onPressIn={this._onPressIn.bind(this)}
-            onPressOut={this._onPressOut.bind(this)}
-            key={i}>
-            <View
-              style={{
-                width: Dimensions.get('window').width / 2,
-                height: Dimensions.get('window').height / 4,
-                backgroundColor: color,
-                transform: [{ scale: this.state.transformed ? 0.5 : 1 }],
-              }}
-            />
-          </TouchableWithoutFeedback>
-        )}
-      </View>
+      <TouchableWithoutFeedback
+        onPressIn={this._onPressIn.bind(this)}
+        onPressOut={this._onPressOut.bind(this)}>
+        <View
+          style={{
+            width: Dimensions.get('window').width / 2,
+            height: Dimensions.get('window').height / 4,
+            backgroundColor: this.props.color,
+            transform: [{ scale: this.state.transformed ? 0.5 : 1 }],
+          }}
+        />
+      </TouchableWithoutFeedback>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{ flexWrap: 'wrap', backgroundColor: 'black' }}>
+        <StatusBar hidden={true} />
+        {colors.map((color, i) => <Tile key={i} color={color} />)}
+      </View>
+    );
+  }
+}
