909edc34badb1da227e312951a821d0e6bf66fa3
Abi Raja <abimanyuraja@gmail.com>
2017-07-21 16:04:34 -0700

    Spinner working

--- App.js
+++ App.js
@@ -7,9 +7,11 @@ import {
   StatusBar,
   TouchableWithoutFeedback,
   Animated,
+  Easing,
 } from 'react-native';
 import { Audio } from 'expo';
 import Mic from './assets/svgs/mic';
+import Spinner from './assets/svgs/spinner';
 
 var tiles = [
   { color: '#FF482F', sound: require('./assets/sounds/clap.wav') },
@@ -23,7 +25,11 @@ var tiles = [
 ];
 
 class Tile extends React.Component {
-  state = { scale: new Animated.Value(1) };
+  state = {
+    scale: new Animated.Value(1),
+    type: 'preset',
+    spinnerRotate: new Animated.Value(0),
+  };
   springOptions = {
     speed: 50,
     bounciness: 8,
@@ -33,37 +39,67 @@ class Tile extends React.Component {
   componentWillMount() {
     this.soundObject = new Audio.Sound();
     this.soundObject.loadAsync(this.props.sound);
+    this.setState({ type: this.props.type });
   }
 
   _onPressIn() {
-    this.soundObject.stopAsync().then((success, error) => {
-      this.soundObject.playAsync();
-    });
+    if (this.state.type == 'preset') {
+      this.soundObject.stopAsync().then((success, error) => {
+        this.soundObject.playAsync();
+      });
 
-    if (this.animation) {
-      this.animation.stop();
+      if (this.animation) {
+        this.animation.stop();
+      }
+
+      this.animation = Animated.spring(this.state.scale, {
+        ...this.springOptions,
+        toValue: 0.7,
+      });
+      this.animation.start();
     }
-
-    this.animation = Animated.spring(this.state.scale, {
-      ...this.springOptions,
-      toValue: 0.7,
-    });
-    this.animation.start();
   }
 
   _onPressOut() {
-    // this.setState({ transformed: false });
-    this.animation.stop();
+    if (this.state.type == 'preset') {
+      this.animation.stop();
 
-    this.animation = Animated.spring(this.state.scale, {
-      ...this.springOptions,
-      toValue: 1,
-    });
+      this.animation = Animated.spring(this.state.scale, {
+        ...this.springOptions,
+        toValue: 1,
+      });
 
-    this.animation.start();
+      this.animation.start();
+    } else if (this.state.type == 'custom') {
+      this.setState({ type: 'recording' });
+
+      // Start spinner
+      this.spinnerAnimation = Animated.timing(this.state.spinnerRotate, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      });
+      this.spinnerLoop = Animated.loop(this.spinnerAnimation);
+      this.spinnerLoop.start();
+
+      // Start recording
+      this.recording = new Audio.Recording();
+      this.recording.prepareToRecordAsync().then(() => {
+        this.recording.startAsync();
+      });
+    } else {
+      this.setState({ type: 'preset' });
+      this.spinnerLoop.stop();
+    }
   }
 
   render() {
+    const spin = this.state.spinnerRotate.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    });
+
     return (
       <TouchableWithoutFeedback
         onPressIn={this._onPressIn.bind(this)}
@@ -75,7 +111,7 @@ class Tile extends React.Component {
             backgroundColor: this.props.color,
             transform: [{ scale: this.state.scale }],
           }}>
-          {this.props.type == 'custom' &&
+          {(this.state.type == 'custom' || this.state.type == 'recording') &&
             <Mic
               width={80}
               height={80}
@@ -85,6 +121,16 @@ class Tile extends React.Component {
                 left: Dimensions.get('window').width / 2 / 2 - 40,
               }}
             />}
+          {this.state.type == 'recording' &&
+            <Animated.View
+              style={{
+                position: 'absolute',
+                top: Dimensions.get('window').height / 4 / 2 - 60,
+                left: Dimensions.get('window').width / 2 / 2 - 60,
+                transform: [{ rotate: spin }],
+              }}>
+              <Spinner width={120} height={120} />
+            </Animated.View>}
         </Animated.View>
       </TouchableWithoutFeedback>
     );
@@ -93,13 +139,13 @@ class Tile extends React.Component {
 
 export default class App extends React.Component {
   componentWillMount() {
-    // Audio.setAudioModeAsync({
-    //   allowsRecordingIOS: false,
-    //   interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS, // TODO(Abi): Switch back to INTERRUPTION_MODE_IOS_DO_NOT_MIX
-    //   playsInSilentModeIOS: true,
-    //   shouldDuckAndroid: true, // TODO(Abi): Is this the common behavior on Android?
-    //   interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-    // });
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS, // TODO(Abi): Switch back to INTERRUPTION_MODE_IOS_DO_NOT_MIX
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true, // TODO(Abi): Is this the common behavior on Android?
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+    });
   }
 
   render() {
